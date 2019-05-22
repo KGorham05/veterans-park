681aa3cd88b863de677b40f78cea76c16a09a0c4
Kgor <kevin.gorham@gmail.com>
2019-05-22 12:12:39 -0700

    sized carousel to be full vw and vh

--- client/src/App.js
+++ client/src/App.js
@@ -18,20 +18,10 @@ function AppRouter() {
   return (
     <Router>
       <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/about/">About</Link>
-            </li>
-            <li>
-              <Link to="/users/">Users</Link>
-            </li>
-          </ul>
-        </nav>
+        
         <Carousel></Carousel>
+
+        
         {/* <Route path="/" exact component={Index} />
         <Route path="/about/" component={About} />
         <Route path="/users/" component={Users} /> */}
