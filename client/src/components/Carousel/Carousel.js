98c9b3a6479788b60a85e76902feef29a130c20f
Kgor <kevin.gorham@gmail.com>
2019-05-22 12:00:58 -0700

    changed relative path to point to online img src

--- client/src/components/Carousel/Carousel.js
+++ client/src/components/Carousel/Carousel.js
@@ -5,13 +5,13 @@ const Carousel = props => (
   <div id="carouselExampleControls" className="carousel slide" data-ride="carousel">
     <div className="carousel-inner">
       <div className="carousel-item active">
-        <img src="../../assets/images/fountain-night" className="d-block w-100" alt="fountain at night with statue" />
+        <img src="https://s3-media1.fl.yelpcdn.com/bphoto/765nHWRB41qMX7hW3fApXg/o.jpg" className="d-block w-100" alt="fountain at night with statue" />
       </div>
       <div className="carousel-item">
-        <img src="../../assets/images/fountain-night" className="d-block w-100" alt="2nd image" />
+        <img src="https://s3-media1.fl.yelpcdn.com/bphoto/kjuKX8NQMOXCy0tLb8LMKg/o.jpg" className="d-block w-100" alt="2nd image" />
       </div>
       <div className="carousel-item">
-        <img src="../../assets/images/fountain-night" className="d-block w-100" alt="3rd image" />
+        <img src="https://s3-media1.fl.yelpcdn.com/bphoto/tR4_HqbwixRrEv8eFGkj8w/o.jpg" className="d-block w-100" alt="3rd image" />
       </div>
     </div>
     <a className="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
