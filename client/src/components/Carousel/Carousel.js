de0ecba2134cedbcfe989d7a399ea25246ddafec
Kgor <kevin.gorham@gmail.com>
2019-05-30 21:06:01 -0700

    restyled carousel size and placement

--- client/src/components/Carousel/Carousel.js
+++ client/src/components/Carousel/Carousel.js
@@ -2,33 +2,37 @@ import React from "react";
 import "./Carousel.css";
 
 const Carousel = props => (
-  <div id="carouselControls" className="carousel slide" data-ride="carousel">
-    <div className="carousel-inner">
-      <div className="carousel-item active">
-        <div class="carousel-img-style">
-          <img src="https://s3-media1.fl.yelpcdn.com/bphoto/765nHWRB41qMX7hW3fApXg/o.jpg" className="d-block w-100" alt="fountain at night with statue" />
+
+  <div className="text-center">
+    <div id="carouselControls" className="carousel slide carousel-img-style" data-ride="carousel">
+      <div className="carousel-inner">
+        <div className="carousel-item active">
+          <div>
+            <img src="https://s3-media1.fl.yelpcdn.com/bphoto/765nHWRB41qMX7hW3fApXg/o.jpg" className="d-block w-100" alt="fountain at night with statue" />
+          </div>
+        </div>
+        <div className="carousel-item">
+          <div>
+            <img src="https://s3-media1.fl.yelpcdn.com/bphoto/kjuKX8NQMOXCy0tLb8LMKg/o.jpg" className="d-block w-100" alt="2nd image" />
+          </div>
+        </div>
+        <div className="carousel-item">
+          <div>
+            <img src="https://s3-media1.fl.yelpcdn.com/bphoto/tR4_HqbwixRrEv8eFGkj8w/o.jpg" className="d-block w-100" alt="3rd image" />
+          </div>
         </div>
       </div>
-      <div className="carousel-item">
-        <div class="carousel-img-style">
-          <img src="https://s3-media1.fl.yelpcdn.com/bphoto/kjuKX8NQMOXCy0tLb8LMKg/o.jpg" className="d-block w-100" alt="2nd image" />
-        </div>
-      </div>
-      <div className="carousel-item">
-        <div class="carousel-img-style">
-          <img src="https://s3-media1.fl.yelpcdn.com/bphoto/tR4_HqbwixRrEv8eFGkj8w/o.jpg" className="d-block w-100" alt="3rd image" />
-        </div>
-      </div>
+      <a className="carousel-control-prev" href="#carouselControls" role="button" data-slide="prev">
+        <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span className="sr-only">Previous</span>
+      </a>
+      <a className="carousel-control-next" href="#carouselControls" role="button" data-slide="next">
+        <span className="carousel-control-next-icon" aria-hidden="true"></span>
+        <span className="sr-only">Next</span>
+      </a>
     </div>
-    <a className="carousel-control-prev" href="#carouselControls" role="button" data-slide="prev">
-      <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-      <span className="sr-only">Previous</span>
-    </a>
-    <a className="carousel-control-next" href="#carouselControls" role="button" data-slide="next">
-      <span className="carousel-control-next-icon" aria-hidden="true"></span>
-      <span className="sr-only">Next</span>
-    </a>
   </div>
+
 );
 
 export default Carousel;
